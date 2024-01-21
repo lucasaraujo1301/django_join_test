21367ab8a7edbd662ec6f972e0a4c15c3221861b
Lucas Araujo de Medeiros <lucasaraujo1301@gmail.com>
2024-01-20 22:15:13 -0300

    feat: changing the default id to UUID instead of BigInt

--- core/models.py
+++ core/models.py
@@ -1,3 +1,10 @@
-# from django.db import models
+import uuid
 
-# Create your models here.
+from django.db import models
+
+
+class CustomBaseModel(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    class Meta:
+        abstract = True
